7f151a64f81af8891c6293ead197937d59cce76f
satchel <ambienxyz@gmail.com>
2017-06-21 11:04:51 +0300

    Initial Release

---
+++ CarbonMod.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.Composition;
+using Ambient;
+using Ambient.Visuals;
+using System.Windows.Forms;
+using SharpDX;
+using System;
+
+namespace CarbonMod
+{
+    [Export(typeof(CarbonPlugin))]
+    public class CarbonMod : CarbonPlugin
+    {
+        private TerrainGeneration terrainGenerator;
+
+        #region IPlugin Members
+
+        public void Initialize(World world)
+        {
+            terrainGenerator = new TerrainGeneration(world);
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Ambient Software";
+            }
+        }
+
+        public void GenerateChunk(
+            Int3 chunkOffset,
+            Voxel.Ownership owner,
+            out ushort[] blocks,
+            out ushort[] crust,
+            out byte[] precipitationData,
+            out bool containsVolcano,
+            out bool containsGeyser,
+            out bool containsPotentialSpring,
+            out World.LifeMode lifeMode)
+        {
+            terrainGenerator.GenerateChunk(chunkOffset, owner, out blocks, out crust, out precipitationData, out containsVolcano, out containsGeyser, out containsPotentialSpring, out lifeMode);
+        }
+
+        public Block GetSuitableLivingBlock(Random random,
+            int weightedPrecipitation,
+            float temperatureWinter,
+            float temperatureSummer,
+            float chaos,
+            Block.LivingGenerationModes vegetationType)
+        {
+            return terrainGenerator.GetSuitableLivingBlock(random, weightedPrecipitation, temperatureWinter, temperatureSummer, chaos, vegetationType);
+        }
+        #endregion
+    }
+}
---
+++ LICENSE.md
@@ -0,0 +1,5 @@
+### Copyright © 2017 Ambient Software
+
+Ambient Software retains copyright over this source code.
+
+This source is provided to assist in creating mods for Carbon. Any other use is prohibited.
